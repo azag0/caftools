1ae77bf958d797b2cc35045cf3deefef14fcfa65
Jan Hermann <dev@hermann.in>
2017-07-23 14:11:54 +0200

    add python packages files

---
+++ README.md
@@ -0,0 +1,3 @@
+# `caftools`
+
+This is a loose collection of Python 3.6 modules for computational chemistry.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='caf',
+    version='0.1.0',
+    url='https://github.com/azag0/caftools',
+    author='Jan Hermann',
+    author_email='dev@hermann.in',
+    license='MIT',
+    packages=['caftools'],
+    scripts=['caf'],
+    classifieres=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+    ],
+)
