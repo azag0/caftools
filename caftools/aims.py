ad9f6e8e9df265b6f825a508f5874229e2775d0a
Jan Hermann <dev@hermann.in>
2017-07-24 12:41:05 +0200

    add AimsWriter

--- caftools/aims.py
+++ caftools/aims.py
@@ -88,3 +88,67 @@ class AimsTask:
             ('control.in', control),
             ('geometry.in', geometry),
         ])
+
+
+def _kwid(**kw):
+    return kw
+
+
+class AimsWriter:
+    rules = {
+        'ROOT': lambda species: species,
+        'species': lambda label, basis, angular_grids, valence, ion_occ, **kw: [
+            ('species', label), kw, angular_grids, valence, ion_occ, basis
+        ],
+        'cut_pot': lambda onset, width, scale: (onset, width, scale),
+        'radial_base': lambda number, radius: (number, radius),
+        'angular_grids': lambda shells: [('angular_grids', 'specified'), shells],
+        'shells': lambda points, radius=None: (
+            ('division', radius, points) if radius
+            else ('outer_grid', points)
+        ),
+        'valence': lambda n, l, occupation: ('valence', n, l, occupation),
+        'ion_occ': lambda n, l, occupation: ('ion_occ', n, l, occupation),
+        'basis': lambda type, n, l, radius=None, z_eff=None: (
+            (type, n, l, radius) if type == 'ionic'
+            else (type, n, l, z_eff)
+        ),
+    }
+
+    def __init__(self, rules=None, root='ROOT'):
+        self.root = root
+        if rules:
+            self.rules = rules
+
+    def stringify(self, value):
+        if isinstance(value, bool):
+            return f'.{str(value).lower()}.'
+        if isinstance(value, tuple):
+            return ' '.join(self.stringify(x) for x in value)
+        if isinstance(value, list):
+            return '\n'.join(self.stringify(x) for x in value)
+        if isinstance(value, dict):
+            return '\n'.join(
+                f'{k} {self.stringify(v)}' if v is not None else k
+                for k, v in sorted(value.items())
+            )
+        return str(value)
+
+    def _transform_value(self, val, rule):
+        if isinstance(val, list):
+            return [self._transform_value(x, rule) for x in val]
+        if isinstance(val, dict):
+            return self.rules.get(rule, _kwid)(**self._transform_node(val))
+        return val
+
+    def _transform_node(self, node):
+        if isinstance(node, dict):
+            return {k: self._transform_value(v, k) for k, v in node.items()}
+        return node
+
+    def transform(self, node):
+        return self._transform_node({self.root: node})[self.root]
+
+    def write(self, node):
+        value = self.transform(node)
+        return self.stringify(value)
